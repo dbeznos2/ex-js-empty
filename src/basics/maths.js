e7fc85f549787441c1079ee9bbb6b82ed96a861a
dbeznos2 <dmytro.beznoskovych@jobtrek.ch>
2023-12-05 10:57:35 +0100

    maths.js

--- src/basics/maths.js
+++ src/basics/maths.js
@@ -3,7 +3,12 @@
  * @return {number} sphere volume
  */
 export function computeSphereVolume(diameter) {
-  //
+    if (typeof diameter !== 'number' || isNaN(diameter) || diameter < 0) {
+        throw new Error('Unsupported type or negative diameter');
+    }
+
+    const radius = diameter / 2;
+    return (4 / 3) * Math.PI * Math.pow(radius, 3);
 }
 
 /**
@@ -11,7 +16,11 @@ export function computeSphereVolume(diameter) {
  * @return {number} number rounded to one decimal
  */
 export function roundNumberToOneDecimals(n) {
-  //
+    if (typeof n !== 'number' || isNaN(n)) {
+        throw new Error('Unsupported type');
+    }
+
+    return Math.round(n * 10) / 10;
 }
 
 /**
@@ -19,13 +28,23 @@ export function roundNumberToOneDecimals(n) {
  * @return {number} average with full precision
  */
 export function computeAverage(grades) {
-  //
+    if (!Array.isArray(grades) || grades.some(value => typeof value !== 'number' || Number.isNaN(value))) {
+        throw new Error('Unsupported type or invalid array');
+    }
+
+    const sum = grades.reduce((acc, grade) => acc + grade, 0);
+    return sum / grades.length;
 }
+
 
 /**
  * @param {number[]} grades An array containing all grades
  * @return {number} rounded average to 1 decimal
  */
 export function roundedAverage(grades) {
-  //
+    if (!Array.isArray(grades) || grades.some(isNaN)) {
+        throw new Error('Unsupported type or invalid array');
+    }
+    const average = computeAverage(grades);
+    return roundNumberToOneDecimals(average);
 }
