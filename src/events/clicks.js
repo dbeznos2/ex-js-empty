afb585cbd6cadf17404e45a700446c6fce70910c
dbeznos2 <dmytro.beznoskovych@jobtrek.ch>
2023-12-05 14:10:31 +0100

    clicks.js

--- src/events/clicks.js
+++ src/events/clicks.js
@@ -4,7 +4,11 @@
  * The alert text should contain "Button clicked"
  */
 export function reactToClickEvent() {
-  //
+  const clickMe = document.getElementById('click-me');
+
+    clickMe.addEventListener('click', function () {
+        alert('Button clicked');
+    });
 }
 
 /**
@@ -12,5 +16,15 @@ export function reactToClickEvent() {
  * But this time you should add a new div below the button with the content "clicked"
  */
 export function addEventToDomOnClick() {
-  //
+    const clickMeElement = document.getElementById('click-me');
+    clickMeElement.addEventListener('click', function () {
+
+        const newDivElement = document.createElement('div');
+
+        newDivElement.textContent = 'clicked';
+
+        clickMeElement.parentNode.appendChild(newDivElement);
+
+    });
+
 }
